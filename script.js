34e37063107150877ec019d28af752386f449344
Yasi Moshtael <yasi.moshtael@gmail.com>
2021-05-18 15:49:36 -0700

    Attended office hours with Bryan today, who very kindly took the time to review the work I had already done, guide me through the remaining steps, and help me work through the points where I found myself stuck. This really helped me develop a better understanding of how to correctly approach this assignment and realize my own missteps in the process! The updates that were made include: updating the if statements to display an alert when user’s password length didn’t meet parameters, create a confirm to ask if users wanted to include lowercase characters, create a conditional that adds the character array to a new global variable with the concat method, create a for loop to generate a random password, and create a function that holds all of those functions — making the starter code write the newly generated password on the page

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ var lowChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", 
 var uppChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var numChar = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var speChar = ["!", "@", "#", "$", "%", "&", "*", "?"];
+var finalArray = [];
 
 // Checking if new arrays are working by printing them in the console
 console.log(lowChar);
@@ -13,27 +14,32 @@ console.log(uppChar);
 console.log(numChar);
 console.log(speChar);
 
+function generatePassword(){
+
 
 // PASSWORD LENGTH
 // prompt user for desired password length 
 var userLength = window.prompt("How many characters long would you like your password to be?");
+console.log(userLength);
 // create conditionals for parameters
   // if user input is not a number, reject
   // if user input is < 8, reject
   if (userLength < 8) {
-    return;
+    window.alert("Your password is too short!");
   }
   // if user input is > 128, reject
   if (userLength > 128) {
-    return;
+    window.alert("Your password is too long!");
   }
-// store user response (length) in variable
-var passLength = userLength.value();
+// // store user response (length) in variable
+// var passLength = userLength.value();
 
 
 // LOWERCASE CHARACTERS
+var userLower = window.confirm("Do you want lowercase characters in your password?");
+console.log(userLower);
   // variable to store the user's choice about lowercase characters = answer to their confirm
-  var userLower = confirm.value();
+  // var userLower = confirm.value();
   // "do you want lowercase characters"
 
 // UPPERCASE CHARACTERS
@@ -49,12 +55,25 @@ var passLength = userLength.value();
     // "do you want special characters"
 
 // if all character confirms are false, tell user they must pick at least one group and return
+if (userLower === true) {
+  finalArray = finalArray.concat(lowChar);
+  console.log(finalArray);
+}
 
 // function to randomly generate password (array)
   // var options = Math.floor(Math.random() * array.length)
   // return options
 
+  var password = "";
+
+  // for loop that loops through the amount of times
+  for (var i = 0; i < userLength; i++) {
+    password = password + finalArray[Math.floor(Math.random() * finalArray.length)];
+  }
+  console.log(password);
+
 // function to generate password 
+return password;
   // variable = passsword options function
   // variable that equals an empty array, array = []
   // check if options exist, if not, return
@@ -72,7 +91,7 @@ var passLength = userLength.value();
   // check if options has special characters
 
 // use the concat method 
-  
+}
 
 // Write password to the #password text area
 function writePassword() {
