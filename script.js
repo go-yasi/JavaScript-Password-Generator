484f1fe019e1d9f7d0871128c02e29901022eaa1
Yasi Moshtael <yasi.moshtael@gmail.com>
2021-05-18 16:33:53 -0700

    removed all old comments, then went through the working code and added new comments to explain the purpose of every function. was a helpful step to solidify my understanding of each line of code

--- script.js
+++ script.js
@@ -8,55 +8,73 @@ var numChar = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var speChar = ["!", "@", "#", "$", "%", "&", "*", "?"];
 var finalArray = [];
 
-// Checking if new arrays are working by printing them in the console
-console.log(lowChar);
-console.log(uppChar);
-console.log(numChar);
-console.log(speChar);
+    // console log to check
+    console.log(lowChar);
+    console.log(uppChar);
+    console.log(numChar);
+    console.log(speChar);
+
 
 function generatePassword(){
-
 
 // PASSWORD LENGTH
 // prompt user for desired password length 
 var userLength = window.prompt("How many characters long would you like your password to be?");
-console.log(userLength);
 // create conditionals for parameters
-  // if user input is not a number, reject
   // if user input is < 8, reject
   if (userLength < 8) {
-    window.alert("Your password is too short!");
+    window.alert("Uh oh! Your password is too short. Please try again.");
+    return;
   }
   // if user input is > 128, reject
   if (userLength > 128) {
-    window.alert("Your password is too long!");
+    window.alert("Uh oh! Your password is too long. Please try again.");
+    return;
   }
-// // store user response (length) in variable
-// var passLength = userLength.value();
 
 
 // LOWERCASE CHARACTERS
+// ask user if they want lowercase characters
 var userLower = window.confirm("Do you want lowercase characters in your password?");
 console.log(userLower);
-  // variable to store the user's choice about lowercase characters = answer to their confirm
-  // var userLower = confirm.value();
-  // "do you want lowercase characters"
 
 // UPPERCASE CHARACTERS
-  // variable to store the user's choice about uppercase characters = answer to their confirm
-    // "do you want uppercase characters"
+// ask user if they want uppercase characters
+var userUpper = window.confirm("Do you want uppercase characters in your password?");
+console.log(userUpper);
 
 // NUMERIC CHARACTERS
-  // variable to store the user's choice about numeric characters = answer to their confirm
-    // "do you want numeric characters"
+// ask user if they want numeric characters
+var userNumeric = window.confirm("Do you want numeric characters in your password?");
+console.log(userNumeric);
 
 // SPECIAL CHARACTERS
-  // variable to store the user's choice about special characters = answer to their confirm
-    // "do you want special characters"
+// ask user if they want special characters
+var userSpecial = window.confirm("Do you want special characters in your password?");
+console.log(userSpecial);
 
-// if all character confirms are false, tell user they must pick at least one group and return
+
+// if user says ok to lowercase characters, add lowChar array to finalArray 
 if (userLower === true) {
   finalArray = finalArray.concat(lowChar);
+  console.log(finalArray);
+}
+
+// if user says ok to uppercase characters, add uppChar array to finalArray 
+if (userUpper === true) {
+  finalArray = finalArray.concat(uppChar);
+  console.log(finalArray);
+}
+
+// if user says ok to numeric characters, add numChar array to finalArray 
+if (userNumeric === true) {
+  finalArray = finalArray.concat(numChar);
+  console.log(finalArray);
+}
+
+// if user says ok to special characters, add speChar array to finalArray 
+if (userSpecial === true) {
+  finalArray = finalArray.concat(speChar);
   console.log(finalArray);
 }
 
@@ -64,33 +82,17 @@ if (userLower === true) {
   // var options = Math.floor(Math.random() * array.length)
   // return options
 
+// password varaible set to empty strin
   var password = "";
 
-  // for loop that loops through the amount of times
+// for loop that generates random characters from finalArray, limited to userLength number
   for (var i = 0; i < userLength; i++) {
     password = password + finalArray[Math.floor(Math.random() * finalArray.length)];
   }
   console.log(password);
 
-// function to generate password 
+// generate password 
 return password;
-  // variable = passsword options function
-  // variable that equals an empty array, array = []
-  // check if options exist, if not, return
-                        // you can use thruthie and falsie values (example below)
-
-                          // if (options) {
-
-                          // }
-                          // if (!options) {
-
-                          // }
-  // check if options has lowercase characters
-  // check if options has uppercase characters
-  // check if options has numeric characters
-  // check if options has special characters
-
-// use the concat method 
 }
 
 // Write password to the #password text area
